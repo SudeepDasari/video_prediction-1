4c1e875c6de1e742ec0c62e4c458853e7d102a5e
Sudeep Dasari <sdasari@berkeley.edu>
2018-11-27 12:13:05 -0800

    created agent

--- scripts/expert_pushing.py
+++ scripts/expert_pushing.py
@@ -2,10 +2,32 @@ from sim.agent.general_agent import GeneralAgent
 from sim.policy.pushing_policy import PushingPolicy
 from sim.mujoco_env.cartgripper_env.cartgripper_xz_grasp import CartgripperXZGrasp
 
+BASE_DIR='/home/sudeep/Desktop/video_prediction-1/data/sim_data'
+env_params = {
+        # resolution sufficient for 16x anti-aliasing
+        'viewer_image_height': 96,
+        'viewer_image_width': 128,
+        'cube_objects': True
+}
+agent_params = {
+    'env': (CartgripperXZGrasp, env_params),
+    'data_save_dir': BASE_DIR,
+    'T': 30,
+    'image_height': 48,
+    'image_width': 64,
+    'gen_xml': 1,  # generate xml every nth trajecotry
+    'record': BASE_DIR + '/record/',
+    'rejection_sample': 5
+}
+
 
 def main():
-    env = CartgripperXZGrasp({})
+    num_collect = 100
+    agent = GeneralAgent(agent_params)
+    policy = PushingPolicy({})
 
+    for i in range(num_collect):
+        agent.sample(policy, i)
 
 if __name__ == '__main__':
     main()
